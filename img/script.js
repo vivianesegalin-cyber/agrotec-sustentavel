b769b0e65e4d480edc624cc11efcdedd3faad46a
vivianesegalin-cyber <viviane.segalin@escola.pr.gov.br>
2026-06-09 14:46:43 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,17 @@
+let atual = 0;
 
+const etapas =
+document.querySelectorAll(".etapa");
+
+function proximaEtapa(){
+
+etapas[atual].classList.remove("ativa");
+
+atual++;
+
+if(atual >= etapas.length){
+atual = 0;
+}
+
+etapas[atual].classList.add("ativa");
+}
