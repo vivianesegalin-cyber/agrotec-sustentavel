2aa80ca88d5e538780469d749f08877dbc5708cf
vivianesegalin-cyber <viviane.segalin@escola.pr.gov.br>
2026-06-11 15:15:26 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,52 +1,10 @@
-function mostrarInfo(tipo){
+// Substitua pelo seu nome de usuário do GitHub
+const githubUser = "SEU-USUARIO";  
+const repoName = "agrotec-sustentavel";
 
-if(tipo === 'regenerativa'){
-alert(
-'A agricultura regenerativa melhora a saúde do solo, aumenta a biodiversidade e reduz a erosão.'
-);
-}
+// Monta o link automaticamente
+const siteUrl = `https://${githubUser}.github.io/${repoName}/`;
 
-if(tipo === 'compostagem'){
-alert(
-'A compostagem transforma restos orgânicos em adubo rico em nutrientes.'
-);
-}
-
-if(tipo === 'agua'){
-alert(
-'O uso eficiente da água reduz desperdícios e protege os recursos hídricos.'
-);
-}
-
-}
-
-const contadores = document.querySelectorAll('.contador');
-
-contadores.forEach(contador => {
-
-const atualizar = () => {
-
-const alvo = +contador.getAttribute('data-target');
-
-const atual = +contador.innerText;
-
-const incremento = alvo / 100;
-
-if(atual < alvo){
-
-contador.innerText =
-Math.ceil(atual + incremento);
-
-setTimeout(atualizar,20);
-
-}else{
-
-contador.innerText = alvo;
-
-}
-
-};
-
-atualizar();
-
-});
+// Atualiza o link na página
+document.getElementById("siteLink").href = siteUrl;
+document.getElementById("siteLink").textContent = `Acessar ${siteUrl}`;
